f3dec8bcec2d6f0d86ac2f781027cddaa557e3e3
samuel922 <onyangosamuel583@gmail.com>
2026-05-26 04:15:42 +0300

    wrapped van details in a div

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -4,7 +4,10 @@ export default function VanDetail() {
     return (
         <div className="container">
             <h1>Van detail page goes here</h1>
-            <p>This is where the van detail information will be displayed.</p>
+            <div className="van-details">
+                <p>This is where the van detail information will be displayed.</p>
+            </div>
+            
         </div>
     )
 }
