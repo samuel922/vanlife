cf261a99b9922522fd722b34606a23c31382411a
samuel922 <onyangosamuel583@gmail.com>
2026-05-27 00:03:40 +0300

    applied stashed changes from feature/van-details

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -6,6 +6,7 @@ export default function VanDetail() {
             <h1>Van detail page goes here</h1>
             <div className="van-details">
                 <p>This is where the van detail information will be displayed.</p>
+                <p> This is a demonstration on git stashing</p>
             </div>
             
         </div>
