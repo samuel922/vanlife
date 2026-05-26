d371df1676853fa83d9f645d28240b05f70e7722
samuel922 <onyangosamuel583@gmail.com>
2026-05-26 03:49:17 +0300

    Add a placeholder for van details

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -4,6 +4,7 @@ export default function VanDetail() {
     return (
         <div className="container">
             <h1>Van detail page goes here</h1>
+            <p>This is where the van detail information will be displayed.</p>
         </div>
     )
 }
